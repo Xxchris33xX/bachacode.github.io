948edf12b3198fed5b9c83871c07058aa7153189
Xxchris33xX <Xxchris33xX@gmail.com>
2022-09-07 13:01:12 -0400

    burger menu, projects, links

--- public/js/main.js
+++ public/js/main.js
@@ -6,14 +6,34 @@ function updateList() {
 	document.querySelectorAll(".nav-option")[[...document.querySelectorAll('h1')].indexOf(titles[0])].classList.add("selected-option");
 }
 
+function scrollFunction() {
+	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+		document.getElementById("navbar").classList.add("scrolled-navbar");
+	} else {
+		document.getElementById("navbar").classList.remove("scrolled-navbar");
+	}
+}
+
 updateList();
 window.addEventListener('scroll', () => {
     updateList();
+	scrollFunction();
 })
 
 
 function methods(){
 	return {
+		menuNav(){
+			 this.open = false
+		},
+		openMenu(){
+			this.open = true
+			document.body.classList.add("scrolling");
+		},
+		closeMenu(){
+			this.open = false
+			document.body.classList.remove("scrolling");
+		},
 		getSkills(){
 			this.frontend = true,
 			this.backend = false,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.html"],
+  content: ["./*.html"],
   theme: {
     extend: {
       spacing: {
         'skills': '36rem',
+        'title-box':'46rem',
       },
       fontFamily:{
         titles: ['DotGothic16'],
         text: ['JetBrains Mono'],
       },
       colors:{
-        'body': '#2c3848',
-        'primary': '#e49f17',
-        'secondary':'#f8f7da',
-        'third':'#953f10',
+        'body': '#61a6ab',
+        'primary': '#ffce6d',
+        'secondary':'#f6f6e9',
+        'item':'#291b25',
+        'highlight':'#ed5b2d',
       }
     },
   },
